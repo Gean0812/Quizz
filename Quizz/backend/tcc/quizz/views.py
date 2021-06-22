f5d2e8f7032eab9127378d0d60fbaa64f7e9ce10
Giovannisb <giovannisb59@gmail.com>
2021-06-22 12:13:11 -0300

    arquivos

---
+++ Quizz/backend/tcc/quizz/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+
+
+def inicio(request):
+    return render(request, 'inicio.html')
+
+def quiz(request):
+    pass
